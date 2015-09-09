19aa7f0761f91080b0caa0396dea59dd266c46c3
Maxime Bedard <maxime.bedard@shopify.com>
2015-09-09 18:32:05 -0400

    Initial commit

---
+++ src/main/java/ca/etsmtl/log720/lab1/App.java
@@ -0,0 +1,13 @@
+package ca.etsmtl.log720.lab1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
